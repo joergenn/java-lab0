00fcd699270f87f6a09a0a9b6df81fc324fe3060
joergenn <andrian1993a@gmail.com>
2023-10-01 22:18:49 +0300

    Update Main.java

--- Lab4/src/main/java/lab4/Main.java
+++ Lab4/src/main/java/lab4/Main.java
@@ -7,7 +7,7 @@ public class Main {
                 .setAddress("")
                 .setPhoneNumber("3809500000009999999")
                 .setIsInsuranced(true);
-
+// test
         Employee employee1 = employeeBuilder.build();
         System.out.println(employee1);
 
